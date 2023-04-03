b99be0da328e74a0319732a55f1a566dd13e04b7
Saliha Oguz <118985690+salihaoguz11@users.noreply.github.com>
2023-04-03 14:37:27 +0100

    blog form textfield

--- src/components/blog/BlogForm.jsx
+++ src/components/blog/BlogForm.jsx
@@ -1,4 +1,13 @@
-import { TextField, Box } from "@mui/material";
+import {
+  Box,
+  Button,
+  FormControl,
+  InputLabel,
+  MenuItem,
+  Select,
+  TextField,
+  Typography,
+} from "@mui/material";
 
 const BlogForm = () => {
   return (
@@ -7,18 +16,74 @@ const BlogForm = () => {
         display: "flex",
         flexDirection: "column",
         gap: 2,
-        width: "100%",
       }}
       component="form"
+      width="100%"
     >
+      <Typography variant="h5" align="left" sx={{ fontWeight: 900 }}>
+        New Blog
+      </Typography>
+
       <TextField
-        label="title"
+        label="Title"
         name="title"
         id="title"
         type="text"
         variant="outlined"
         required
-      ></TextField>
+      />
+      <TextField
+        label="Image Url"
+        name="img-url"
+        id="img-url"
+        type="url"
+        variant="outlined"
+        required
+      />
+
+      <FormControl fullWidth>
+        <InputLabel id="demo-simple-select-label">Categories</InputLabel>
+        <Select
+          labelId="demo-simple-select-label"
+          id="demo-simple-select"
+          //value={age}
+          label="Categories"
+          //onChange={handleChange}
+        >
+          <MenuItem value={10}>Trivia</MenuItem>
+          <MenuItem value={20}>Travel</MenuItem>
+          <MenuItem value={30}>Web Development</MenuItem>
+        </Select>
+      </FormControl>
+
+      <FormControl fullWidth>
+        <InputLabel id="demo-simple-select-label">Status</InputLabel>
+        <Select
+          labelId="demo-simple-select-label"
+          id="demo-simple-select"
+          //value={age}
+          label="Status"
+          //onChange={handleChange}
+        >
+          <MenuItem value={10}>Please Chose...</MenuItem>
+          <MenuItem value={20}>Draft</MenuItem>
+          <MenuItem value={30}>Published</MenuItem>
+        </Select>
+      </FormControl>
+
+      <TextField
+        label="Content"
+        name="content"
+        id="content"
+        type="text"
+        variant="outlined"
+        required
+        multiline
+        size="medium"
+        rows={2}
+      />
+
+      <Button variant="contained">ADD NEW BLOG</Button>
     </Box>
   );
 };
