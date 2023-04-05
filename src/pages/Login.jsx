ae8ad79dad87a4eaf91a8d349d5008fc10660fe7
Saliha Oguz <118985690+salihaoguz11@users.noreply.github.com>
2023-04-05 14:36:55 +0100

    details

---
+++ src/pages/Login.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Login = () => {
+  return (
+    <Container>
+      <Grid container>
+        <Grid item></Grid>
+      </Grid>
+    </Container>
+  );
+};
+
+export default Login;
