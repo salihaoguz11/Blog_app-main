9c0abbeb244a637ba55062dbbdfa5b38864ed80c
Saliha Oguz <118985690+salihaoguz11@users.noreply.github.com>
2023-04-17 11:51:09 +0100

    https

--- src/hooks/useAxios.jsx
+++ src/hooks/useAxios.jsx
@@ -3,10 +3,10 @@ import axios from "axios";
 const useAxios = () => {
   const token = "f0a29c03bc786b2dc5941e95a1c650fe9b51418e";
   const axiosPublic = axios.create({
-    baseURL: "http://32131.fullstack.clarusway.com/",
+    baseURL: "https://32131.fullstack.clarusway.com/",
   });
   const axiosWithToken = axios.create({
-    baseURL: "http://32131.fullstack.clarusway.com/",
+    baseURL: "https://32131.fullstack.clarusway.com/",
     headers: { Authorization: `Token ${token}` },
   });
   return { axiosPublic, axiosWithToken };
